0af78db23907d077a6d2e8174f40b783e28b3fbc
Sudhu145 <smartsudhakar145@gmail.com>
2023-08-13 22:00:07 +0530

    Create "Article003_WonderingWhatIsBitBanding" Project

---
+++ Article003_WonderingWhatIsBitBanding/Inc/main.h
@@ -0,0 +1,26 @@
+/*
+ * main.h
+ *
+ *  Created on: Aug 12, 2023
+ *      Author: SUDHAKAR
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define BIT_BAND_REGION_BASE_ADDR 				(0x20000000UL)
+#define BIT_BAND_REGION_END_ADDR      			(0x200FFFFFUL)
+#define BIT_BAND_ALIAS_REGION_BASE_ADDR 		(0x22000000UL)
+#define BIT_BAND_ALIAS_ADDR_EACH_BYTE_OFFSET	(32U)
+#define BIT_BAND_ALIAS_ADDR_EACH_BIT_OFFSET		(4U)
+
+#define TRUE									(0x01U)
+#define FALSE									(0X00U)
+
+#define NULL_PTR								(0)
+
+#define LSB_BIT_MASK							(0x01U)
+
+
+
+#endif /* MAIN_H_ */
